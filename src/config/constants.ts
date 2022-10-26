17576bb055c00dedd4302c46b218534bafe1bdb3
andreyc21 <st21net@mail.ru>
2022-10-27 01:26:27 +0300

    fix lint

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,8 @@ import chains from './chains'
 
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
-export const GATEWAY_URL_PRODUCTION = process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-config.lachain.net'
+export const GATEWAY_URL_PRODUCTION =
+  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-config.lachain.net'
 export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-config.lachain.net'
 
 // Magic numbers
