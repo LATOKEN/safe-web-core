9643954385a13e4aaaa5b83e511ab4f30fc071ba
andreyc21 <st21net@mail.ru>
2022-11-08 13:11:13 +0300

    test

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,8 +3,8 @@ import chains from './chains'
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
 export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-config.lachain.net'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-config.lachain.net'
+  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-client.lachain.net'
+export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-client.lachain.net'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
